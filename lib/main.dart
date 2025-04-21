e21dfcf11b70bab4374753ec6c69b0c0b7640d49
Humberto Junior <astronomi@gmail.com>
2025-04-21 12:14:16 -0300

    Updated main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:tech_taste/data/restaurant_data.dart' show RestaurantData;
+import 'package:tech_taste/model/restaurant.dart';
 
 import 'package:tech_taste/ui/_core/app_theme.dart';
 import 'package:tech_taste/ui/home/home_screen.dart';
 import 'package:tech_taste/ui/splash/splash_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  RestaurantData restaurantData = RestaurantData();
+  await restaurantData.getRestaurants();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) {
+            return restaurantData;
+          },
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
