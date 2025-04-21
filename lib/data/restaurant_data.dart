e01f25850ce543a8102e40e87d50553b85f3b722
Humberto Junior <astronomi@gmail.com>
2025-04-21 12:16:07 -0300

    Created restaurant data

--- lib/data/restaurant_data.dart
+++ lib/data/restaurant_data.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 
 import 'package:tech_taste/model/restaurant.dart';
 
-class RestaurantData {
-  Future<List<Restaurant>> getRestaurants() async {
-    List<Restaurant> listRestaurant = [];
-
+class RestaurantData extends ChangeNotifier {
+  List<Restaurant> listRestaurant = [];
+  Future<void> getRestaurants() async {
     String jsonString = await rootBundle.loadString('assets/data.json');
 
     Map<String, dynamic> data = json.decode(jsonString);
@@ -15,7 +15,5 @@ class RestaurantData {
     for (int i = 0; i < restaurantsData.length; i++) {
       listRestaurant.add(Restaurant.fromMap(restaurantsData[i]));
     }
-
-    return listRestaurant;
   }
 }
