ccad2511fb18be6ebc0d56f4892d78ca3e616920
Humberto Junior <astronomi@gmail.com>
2025-04-21 12:14:43 -0300

    Updated home screen

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:tech_taste/data/categories_data.dart';
+import 'package:tech_taste/data/restaurant_data.dart';
+import 'package:tech_taste/model/restaurant.dart';
 import 'package:tech_taste/ui/home/widgets/category_widget.dart';
+import 'package:tech_taste/ui/home/widgets/restaurant_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    RestaurantData restaurantData = Provider.of<RestaurantData>(context);
     return Scaffold(
       drawer: Drawer(),
       appBar: AppBar(
@@ -45,6 +51,18 @@ class HomeScreen extends StatelessWidget {
 
               Image.asset('assets/banners/banner_promo.png'),
               Text('Bem avaliados'),
+
+              Column(
+                spacing: 8.0,
+                children: List.generate(restaurantData.listRestaurant.length, (
+                  index,
+                ) {
+                  Restaurant restaurant = restaurantData.listRestaurant[index];
+                  return RestaurantWidget(restaurant: restaurant);
+                }),
+              ),
+
+              SizedBox(height: 64.0),
             ],
           ),
         ),
