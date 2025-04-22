5394ed5bf92ddf3c7dcd46d04df464837540a16b
Humberto Junior <astronomi@gmail.com>
2025-04-22 19:31:52 -0300

    Updated checkout screen, add, remove and clear

--- lib/checkout/checkout_screen.dart
+++ lib/checkout/checkout_screen.dart
@@ -1,10 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:tech_taste/model/dish.dart';
+import 'package:tech_taste/ui/_core/app_colors.dart';
+import 'package:tech_taste/ui/_core/bag_provider.dart';
 
 class CheckoutScreen extends StatelessWidget {
   const CheckoutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    BagProvider bagProvider = Provider.of<BagProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Car'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              bagProvider.clearBag();
+            },
+            child: Text('Clear'),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text('Orders', textAlign: TextAlign.center),
+              Column(
+                children: List.generate(
+                  bagProvider.getMapByAmount().keys.length,
+                  (index) {
+                    Dish dish =
+                        bagProvider.getMapByAmount().keys.toList()[index];
+                    return ListTile(
+                      onTap: () {},
+                      leading: Image.asset(
+                        'assets/dishes/default.png',
+                        width: 52,
+                        height: 52,
+                      ),
+                      title: Text(
+                        dish.name,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text('R\$${dish.price.toStringAsFixed(2)}'),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              bagProvider.removeDish(dish);
+                            },
+                            icon: Icon(Icons.remove),
+                          ),
+                          Text(
+                            bagProvider.getMapByAmount()[dish].toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0,
+                              color: AppColors.mainColor,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              bagProvider.addAllDishes([dish]);
+                            },
+                            icon: Icon(Icons.add),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
